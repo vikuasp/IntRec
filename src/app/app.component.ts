6db0784ebd72439196455831878332c7a6b2b016
Vikash Kumar <vikash8084060357@gamil.com>
2023-04-09 13:24:20 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+declare var window:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  navOpen: boolean = false;
+  dropOpen: boolean = false;
+  studentModal: any;
+  ngOnInit(): void { 
+    this.studentModal = new window.bootstrap.Modal(
+      document.getElementById("exampleModal")
+    );
+  }
+  
+  NavbarOpen(){
+    this.navOpen=!this.navOpen;
+  }
+  DropdownOpen(){
+    this.dropOpen=!this.dropOpen;
+  }
+
+  openModal(){
+    this.studentModal.show();
+  }
+
+  closeModal(){
+    this.studentModal.hide();
+  }
 }
