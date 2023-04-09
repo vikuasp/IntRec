b8f1e62e2b12d0a98a800ed09aa787d83771a795
Vikash Kumar <vikash8084060357@gamil.com>
2023-04-09 20:07:39 +0530

    slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 declare var window:any;
 @Component({
   selector: 'app-root',
@@ -29,5 +30,30 @@ export class AppComponent implements OnInit {
 
   closeModal(){
     this.studentModal.hide();
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<-', '->'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: true
   }
 }
